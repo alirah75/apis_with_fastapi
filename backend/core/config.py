a2b2d461854f28bb30092315ab609725761f730d
alirah75 <alihey750@gmail.com>
2024-10-20 10:29:55 +0330

    1- create project 2- connect to database 3- add alembic to project

---
+++ backend/core/config.py
@@ -0,0 +1,21 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# env_path = Path('.') / '../.env'
+env_path = Path(os.path.abspath('./.env'))
+load_dotenv(dotenv_path=env_path)
+
+
+class Setting:
+    PROJECT_NAME:str = "Job Board 🔥"
+    PROJECT_VERSION:str = "1.0.0"
+
+    MYSQL_USER :str = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+    MYSQL_SERVER :str = os.getenv("MYSQL_SERVER", "localhost")
+    MYSQL_PORT :str = os.getenv("MYSQL_PORT", 3306)
+    MYSQL_DB :str = os.getenv("MYSQL_DB")
+    DATABASE_URL = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}'
+
+setting = Setting()
---
+++ backend/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from core.config import setting
+from db.session import engine
+from db.base_class import Base
+
+import uvicorn
+
+
+def create_table():
+    Base.metadata.create_all(bind=engine)
+
+
+def start_application():
+    app = FastAPI(title=setting.PROJECT_NAME, version=setting.PROJECT_VERSION)
+    create_table()
+    return app
+
+app = start_application()
+
+
+@app.get('/')
+async def root():
+    return {'msg': 'Hello world!🚀'}
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
