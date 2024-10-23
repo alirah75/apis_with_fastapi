3b63507a4227113797d84b76d475857fdb38462a
alirah75 <alihey750@gmail.com>
2024-10-24 02:12:33 +0330

    add support for hashing

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -19,14 +19,13 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from db.base import Base
-from core.config import  setting
+from core.config import setting
 config.set_main_option('sqlalchemy.url', setting.DATABASE_URL)
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+# my_important_option = config.get_main_option("my_important_option")# ... etc.
 
 
 def run_migrations_offline() -> None:
