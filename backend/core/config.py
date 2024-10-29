33925ae705f80892ccaf5e417f3553cf6e5c6a92
alirah75 <alihey750@gmail.com>
2024-10-29 11:50:05 +0330

    JWT token creation

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,19 +3,24 @@ from dotenv import load_dotenv
 from pathlib import Path
 
 # env_path = Path('.') / '../.env'
-env_path = Path(os.path.abspath('./.env'))
+env_path = Path(os.path.abspath("./.env"))
 load_dotenv(dotenv_path=env_path)
 
 
 class Setting:
-    PROJECT_NAME:str = "Job Board 🔥"
-    PROJECT_VERSION:str = "1.0.0"
+    PROJECT_NAME: str = "API Blogs 🔥"
+    PROJECT_VERSION: str = "1.0.0"
 
-    MYSQL_USER :str = os.getenv("MYSQL_USER")
+    MYSQL_USER: str = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
-    MYSQL_SERVER :str = os.getenv("MYSQL_SERVER", "localhost")
-    MYSQL_PORT :str = os.getenv("MYSQL_PORT", 3306)
-    MYSQL_DB :str = os.getenv("MYSQL_DB")
-    DATABASE_URL = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}'
+    MYSQL_SERVER: str = os.getenv("MYSQL_SERVER", "localhost")
+    MYSQL_PORT: str = os.getenv("MYSQL_PORT", 3306)
+    MYSQL_DB: str = os.getenv("MYSQL_DB")
+    DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_SERVER}:{MYSQL_PORT}/{MYSQL_DB}"
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
 
 setting = Setting()
